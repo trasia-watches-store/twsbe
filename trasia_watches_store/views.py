3b8ac0b74b57f9a6c1007069fae8d82b5eb23059
hnhtran <tran_eetas@yahoo.com>
2022-07-26 00:12:58 +1000

    create method get and post

--- trasia_watches_store/views.py
+++ trasia_watches_store/views.py
@@ -7,3 +7,15 @@ from .models import Watch
 from .serializers import *
 
 # Create your views here.
+@api_view(['GET', 'POST'])
+def watches_list(request):
+    if request.method == 'GET':
+        watches = Watch.objects.all()
+        serializer = WatchSerializer(watches, context={'request': request}, many=True)
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer = WatchSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
