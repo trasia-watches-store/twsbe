97945aa3f7d4fd592d3f8d5836676940bf233e48
hnhtran <tran_eetas@yahoo.com>
2022-07-26 00:18:40 +1000

    complete crud in views

--- trasia_watches_store/views.py
+++ trasia_watches_store/views.py
@@ -19,3 +19,21 @@ def watches_list(request):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['PUT', 'DELETE'])
+def watches_detail(request, pk):
+    try:
+        watch = Watch.objects.get(pk=pk)
+    except Watch.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'PUT':
+        serializer = WatchSerializer(watch, data=request.data, context={'request': request})
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_204_NO_CONTENT)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == 'DELETE':
+        watch.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
