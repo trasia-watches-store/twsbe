6d21dc1f546d36290b88193ed507d74963f2b909
hnhtran <tran_eetas@yahoo.com>
2022-07-26 00:08:35 +1000

    views import model and serializers

--- trasia_watches_store/views.py
+++ trasia_watches_store/views.py
@@ -3,4 +3,7 @@ from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 
+from .models import Watch
+from .serializers import *
+
 # Create your views here.
