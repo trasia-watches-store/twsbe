9d37344148f0918347cb5b0cfe0837a5fa369b18
hnhtran <tran_eetas@yahoo.com>
2022-07-26 00:05:28 +1000

    added repath to watches list and detail

--- twsbe/urls.py
+++ twsbe/urls.py
@@ -14,8 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
+from trasia_watches_store import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    re_path(r'^watches/$', views.watches_list, name='watches_list'),
+    re_path('^watches/(?P<slug>[-\w]+)/$', views.watches_detail, name='watches_detail'),
 ]
