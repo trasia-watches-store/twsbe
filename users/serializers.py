5a89d70224da7b102e3404e81d44539a2b7defd6
hnhtran <tran_eetas@yahoo.com>
2022-07-26 23:48:13 +1000

    complete usersserializers

--- users/serializers.py
+++ users/serializers.py
@@ -1,3 +1,11 @@
 from rest_framework.serializers import ModelSerializer
 from .models import CustomUser
 
+class UserSerializer(ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = ('pk', 'email', 'last_login', 'is_staff', 'created_at', 'updated_at')
+        read_only_fields = ('created_at', 'updated_at')
+        extra_kwargs = {
+            'email': {'read_only': True}
+        }
