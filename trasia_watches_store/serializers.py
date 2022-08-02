803ebb4f1102f3fadb9a0481c20e44485398c68d
hnhtran <tran_eetas@yahoo.com>
2022-08-02 11:26:14 +1000

    update serializers

--- trasia_watches_store/serializers.py
+++ trasia_watches_store/serializers.py
@@ -20,6 +20,7 @@ class WatchSerializer(serializers.ModelSerializer):
         model = Watch
         # fields = '__all__'
         fields = ('pk', 'name', 'type', 'features', 'price', 'stockNum', 'brand', 'family', 'model', 'limited', 'water_resistance_depth', 'case_description', 'dial_description', 'movement_description', 'wimage', 'created_at', 'updated_at')
+        # fields = ('pk', 'name', 'type', 'wimage', 'created_at', 'updated_at')
         read_only_fields = ('created_at', 'updated_at')
 
 class WatchesPictureSerializer(serializers.ModelSerializer):
