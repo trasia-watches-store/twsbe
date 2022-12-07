46c573d49ced3017d3a889dfccf2677e56ffdd56
hnhtran <tran_eetas@yahoo.com>
2022-12-07 16:21:33 +1100

    render.yaml

--- twsbe/settings.py
+++ twsbe/settings.py
@@ -30,9 +30,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'RENDER' not in os.environ
 
 ALLOWED_HOSTS = ['https://trasia-watches-store.herokuapp.com/']
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 # Application definition
 
