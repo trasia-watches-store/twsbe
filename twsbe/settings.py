8d72db53a87641b33c77fcbba51962761383a04c
hnhtran <tran_eetas@yahoo.com>
2022-12-11 15:11:17 +1100

    settings

--- twsbe/settings.py
+++ twsbe/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-# This is the setting before winding back to commit heroku
+
 from pathlib import Path
 from dotenv import load_dotenv
 import os
@@ -30,13 +30,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = 'TRUE'
-DEBUG = 'RENDER' not in os.environ
+DEBUG = True
 
-ALLOWED_HOSTS = ['https://trasia-watches-store.onrender.com/']
-RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
-if RENDER_EXTERNAL_HOSTNAME:
-    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
+ALLOWED_HOSTS = []
+
 
 # Application definition
 
